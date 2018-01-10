ecaf0628f037857e4c665facbdf540236d8494e7
Mark Alexander Klingenberg <mark.klingenberg@entelect.co.za>
2018-01-10 19:41:32 +0200

    blink 182

--- comical/src/main/java/za/co/entelect/bootcamp/App.java
+++ comical/src/main/java/za/co/entelect/bootcamp/App.java
@@ -5,6 +5,7 @@ public class App
     public static final double CURRENT_RATE = 22.07;
 
     public float convertRandToDollar(double value){
+        System.out.println("nanananaanan") ;
         return value * CURRENT_RATE / 100 ;
         //testing things
         //git testing things
